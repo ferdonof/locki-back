e6ffb3cbe5ecbbea1e42097a11ba1a39edcc4937
ferdonof <pmfernan@gmail.com>
2026-03-25 21:44:10 +0100

    feat: update user domain entity

--- manage-administration/domain/src/main/java/com/ferdonof/locki/users/entities/LockiUser.java
+++ manage-administration/domain/src/main/java/com/ferdonof/locki/users/entities/LockiUser.java
@@ -1,8 +1,11 @@
 package com.ferdonof.locki.users.entities;
 
+import lombok.Builder;
+
 import java.time.Instant;
 import java.util.UUID;
 
+@Builder(toBuilder = true)
 public record LockiUser(UUID id, String name, String phone, String email, Long version, Instant createdAt,
 		Instant updatedAt) {
 }
