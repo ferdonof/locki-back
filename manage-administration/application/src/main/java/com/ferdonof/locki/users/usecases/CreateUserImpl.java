3333294280200c62e1ca2bba9fb466c8fce029bd
ferdonof <pmfernan@gmail.com>
2026-03-25 09:58:14 +0100

    feat: update use case impl

--- manage-administration/application/src/main/java/com/ferdonof/locki/users/usecases/CreateUserImpl.java
+++ manage-administration/application/src/main/java/com/ferdonof/locki/users/usecases/CreateUserImpl.java
@@ -1,9 +1,10 @@
 package com.ferdonof.locki.users.usecases;
 
-import com.ferdonof.locki.users.entities.LockiUser;
-import com.ferdonof.locki.users.ports.UserRepositoryPort;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import com.ferdonof.locki.users.entities.LockiUser;
+import com.ferdonof.locki.users.ports.UserRepositoryPort;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -13,6 +14,7 @@ public class CreateUserImpl implements CreateUser {
 
 	@Override
 	public LockiUser execute(LockiUser lockiUser) {
+		log.info("Creating user with email {}", lockiUser.email());
 		return this.userRepository.insert(lockiUser);
 
 	}
