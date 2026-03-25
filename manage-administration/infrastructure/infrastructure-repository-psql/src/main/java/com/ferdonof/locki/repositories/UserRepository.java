077b27b327dd8bc234cf2617aa12f9e4ea3ee80f
ferdonof <pmfernan@gmail.com>
2026-03-25 09:41:50 +0100

    feat: change CrudRepository to JpaRepository

--- manage-administration/infrastructure/infrastructure-repository-psql/src/main/java/com/ferdonof/locki/repositories/UserRepository.java
+++ manage-administration/infrastructure/infrastructure-repository-psql/src/main/java/com/ferdonof/locki/repositories/UserRepository.java
@@ -2,9 +2,9 @@ package com.ferdonof.locki.repositories;
 
 import java.util.UUID;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ferdonof.locki.entities.UserEntity;
 
-public interface UserRepository extends CrudRepository<UserEntity, UUID> {
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 }
