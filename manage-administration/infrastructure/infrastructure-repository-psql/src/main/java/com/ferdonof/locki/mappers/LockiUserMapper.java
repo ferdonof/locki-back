7acdfceeb9dff1c64b6d3c151c438d727be2a087
ferdonof <pmfernan@gmail.com>
2026-03-25 21:44:22 +0100

    feat: update mappers

--- manage-administration/infrastructure/infrastructure-repository-psql/src/main/java/com/ferdonof/locki/mappers/LockiUserMapper.java
+++ manage-administration/infrastructure/infrastructure-repository-psql/src/main/java/com/ferdonof/locki/mappers/LockiUserMapper.java
@@ -1,14 +1,19 @@
 package com.ferdonof.locki.mappers;
 
-import com.ferdonof.locki.entities.UserEntity;
-import com.ferdonof.locki.users.entities.LockiUser;
 import org.mapstruct.CollectionMappingStrategy;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.NullValueCheckStrategy;
 import org.mapstruct.NullValueMappingStrategy;
 
-@Mapper(collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED, nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS, componentModel = "spring", nullValueMappingStrategy = NullValueMappingStrategy.RETURN_NULL, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+import com.ferdonof.locki.entities.UserEntity;
+import com.ferdonof.locki.users.entities.LockiUser;
+
+@Mapper(collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED,
+		nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
+		componentModel = "spring",
+		nullValueMappingStrategy = NullValueMappingStrategy.RETURN_NULL,
+		injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface LockiUserMapper {
 
 	LockiUser toDomain(UserEntity userEntity);
