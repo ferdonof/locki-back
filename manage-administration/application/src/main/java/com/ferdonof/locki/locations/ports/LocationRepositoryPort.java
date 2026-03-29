996c5e29c09886a3dc14aaac076b46e891b16d55
ferdonof <pmfernan@gmail.com>
2026-03-29 20:04:13 +0200

    feat: implements repository methods

--- manage-administration/application/src/main/java/com/ferdonof/locki/locations/ports/LocationRepositoryPort.java
+++ manage-administration/application/src/main/java/com/ferdonof/locki/locations/ports/LocationRepositoryPort.java
@@ -2,6 +2,11 @@ package com.ferdonof.locki.locations.ports;
 
 import com.ferdonof.locki.locations.entities.Location;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface LocationRepositoryPort {
   Location insert(Location location);
+
+  Optional<Location> findById(UUID uuid);
 }
