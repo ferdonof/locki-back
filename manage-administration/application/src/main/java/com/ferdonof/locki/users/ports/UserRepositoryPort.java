07b0b5adb27641db5d8a0e0353560b1d9ba63f47
ferdonof <pmfernan@gmail.com>
2026-04-02 18:56:18 +0200

    feat: add new repository methods

--- manage-administration/application/src/main/java/com/ferdonof/locki/users/ports/UserRepositoryPort.java
+++ manage-administration/application/src/main/java/com/ferdonof/locki/users/ports/UserRepositoryPort.java
@@ -1,7 +1,16 @@
 package com.ferdonof.locki.users.ports;
 
 import com.ferdonof.locki.users.entities.LockiUser;
+import com.ferdonof.locki.users.entities.UserFilter;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface UserRepositoryPort {
 	LockiUser insert(LockiUser lockiUser);
+
+	Optional<LockiUser> findById(UUID id);
+
+	List<LockiUser> search(UserFilter filter);
 }
--- manage-administration/infrastructure/infrastructure-repository-psql/src/main/java/com/ferdonof/locki/repositories/LockerRepository.java
+++ manage-administration/infrastructure/infrastructure-repository-psql/src/main/java/com/ferdonof/locki/repositories/LockerRepository.java
@@ -2,8 +2,12 @@ package com.ferdonof.locki.repositories;
 
 import com.ferdonof.locki.entities.LockerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
 import java.util.UUID;
 
-public interface LockerRepository extends JpaRepository<LockerEntity, UUID> {
+public interface LockerRepository extends JpaRepository<LockerEntity, UUID>, JpaSpecificationExecutor<LockerEntity> {
+
+  List<LockerEntity> findByRackId(UUID rackId);
 }
