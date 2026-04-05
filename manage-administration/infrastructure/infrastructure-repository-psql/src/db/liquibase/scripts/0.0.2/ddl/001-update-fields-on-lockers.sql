4125e8a936f2bdfc6070960cc91ad35ed5795931
ferdonof <pmfernan@gmail.com>
2026-04-05 12:11:28 +0200

    feat: update scripts for DB agnostic column alteration

--- manage-administration/infrastructure/infrastructure-repository-psql/src/db/liquibase/scripts/0.0.2/ddl/001-update-fields-on-lockers.sql
+++ manage-administration/infrastructure/infrastructure-repository-psql/src/db/liquibase/scripts/0.0.2/ddl/001-update-fields-on-lockers.sql
@@ -2,8 +2,8 @@
 --changeset pmfernan@gmail.com:lockers_administration_0.0.2_0
 --comment: UPDATE fields on lockers table
 
-ALTER TABLE lockers rename COLUMN "number" TO serial;
+ALTER TABLE lockers RENAME COLUMN number TO serial;
 ALTER TABLE lockers add COLUMN position INT NOT NULL default 0;
 
---rollback ALTER TABLE lockers rename COLUMN serial TO "number";
+--rollback ALTER TABLE lockers RENAME COLUMN serial TO number;
 --rollback ALTER TABLE lockers drop COLUMN position;
--- manage-administration/infrastructure/infrastructure-repository-psql/src/db/liquibase/scripts/0.0.2/ddl/003-update-fields-on-locations.sql
+++ manage-administration/infrastructure/infrastructure-repository-psql/src/db/liquibase/scripts/0.0.2/ddl/003-update-fields-on-locations.sql
@@ -2,8 +2,8 @@
 --changeset pmfernan@gmail.com:locations_administration_0.0.2_0
 --comment: UPDATE fields on locations table
 
-ALTER TABLE locations rename COLUMN latitude TO lat;
-ALTER TABLE locations rename COLUMN longitude TO lon;
+ALTER TABLE locations RENAME COLUMN latitude TO lat;
+ALTER TABLE locations RENAME COLUMN longitude TO lon;
 
---rollback ALTER TABLE locations rename COLUMN lat TO latitude;
---rollback ALTER TABLE locations rename COLUMN lon TO longitude;
+--rollback ALTER TABLE locations RENAME COLUMN lat TO latitude;
+--rollback ALTER TABLE locations RENAME COLUMN lon TO longitude;
