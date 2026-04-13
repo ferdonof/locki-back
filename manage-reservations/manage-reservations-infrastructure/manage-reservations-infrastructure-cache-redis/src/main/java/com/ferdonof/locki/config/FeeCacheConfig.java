dddb1462df26a0e93be3b19862a78dc0ee507626
ferdonof <pmfernan@gmail.com>
2026-04-13 11:52:00 +0200

    feat: configure dependencies and FeeCacheConfig

--- manage-reservations/manage-reservations-infrastructure/manage-reservations-infrastructure-cache-redis/src/main/java/com/ferdonof/locki/config/FeeCacheConfig.java
+++ manage-reservations/manage-reservations-infrastructure/manage-reservations-infrastructure-cache-redis/src/main/java/com/ferdonof/locki/config/FeeCacheConfig.java
@@ -1,8 +1,36 @@
 package com.ferdonof.locki.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class FeeCacheConfig {
 
+  @Bean
+  @ConditionalOnMissingBean
+  public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    final RedisTemplate<String, Object> template = new RedisTemplate<>();
+    template.setConnectionFactory(connectionFactory);
+
+    // String serializer for keys
+    final StringRedisSerializer stringSerializer = new StringRedisSerializer();
+
+    // JSON serializer for values
+    final GenericJackson2JsonRedisSerializer jsonSerializer = new GenericJackson2JsonRedisSerializer();
+
+    // Set serializers
+    template.setKeySerializer(stringSerializer);
+    template.setValueSerializer(jsonSerializer);
+    template.setHashKeySerializer(stringSerializer);
+    template.setHashValueSerializer(jsonSerializer);
+
+    template.afterPropertiesSet();
+    return template;
+  }
 }
+
